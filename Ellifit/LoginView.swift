92bf7c8a558b63502f6b8d47d3f1f3312cc32ace
Rudrank Riyam <rudrankriyam@gmail.com>
2021-12-21 19:40:18 +0530

    Update LoginView

--- Ellifit/LoginView.swift
+++ Ellifit/LoginView.swift
@@ -8,51 +8,44 @@
 import SwiftUI
 
 struct LoginView: View {
-    @EnvironmentObject var viewModel: AuthenticationViewModel
-    
-    var body: some View {
-        VStack {
-            Spacer()
-            
-            Image("header_image")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            
-            Text("Welcome to Ellifit!")
-                .fontWeight(.black)
-                .foregroundColor(Color(.systemIndigo))
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-            
-            Text("Empower your elliptical workouts by tracking every move.")
-                .fontWeight(.light)
-                .multilineTextAlignment(.center)
-                .padding()
-            
-            Spacer()
-            
-            Button("Sign in with Google") {
-                viewModel.signIn()
-            }
-            .buttonStyle(AuthenticationButtonStyle())
+
+  // 1
+  @EnvironmentObject var viewModel: AuthenticationViewModel
+
+  var body: some View {
+    VStack {
+      Spacer()
+
+      // 2
+      Image("header_image")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+
+      Text("Welcome to Ellifit!")
+        .fontWeight(.black)
+        .foregroundColor(Color(.systemIndigo))
+        .font(.largeTitle)
+        .multilineTextAlignment(.center)
+
+      Text("Empower your elliptical workouts by tracking every move.")
+        .fontWeight(.light)
+        .multilineTextAlignment(.center)
+        .padding()
+
+      Spacer()
+
+      // 3
+      GoogleSignInButton()
+        .padding()
+        .onTapGesture {
+          viewModel.signIn()
         }
     }
-}
-
-struct AuthenticationButtonStyle: ButtonStyle {
-    func makeBody(configuration: Self.Configuration) -> some View {
-        configuration.label
-            .foregroundColor(.white)
-            .padding()
-            .frame(maxWidth: .infinity)
-            .background(Color(.systemIndigo))
-            .cornerRadius(12)
-            .padding()
-    }
+  }
 }
 
 struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
+  static var previews: some View {
+    LoginView()
+  }
 }
